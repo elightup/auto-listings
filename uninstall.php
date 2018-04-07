2f607d53e2bc3930f9b3f6a951ef86bb7fedbab2
Anh <anhtnt@elightup.com>
2018-04-07 10:42:57 +0700

    Cleanup

--- uninstall.php
+++ uninstall.php
@@ -1,10 +1,8 @@
 <?php
-/**
- * Uninstall Auto_Listings
- */
-
 // Exit if accessed directly.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit;
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
 
 // Load Auto_Listings file.
 include_once( 'auto-listings.php' );
@@ -12,37 +10,46 @@ include_once( 'auto-listings.php' );
 global $wpdb, $wp_roles;
 
 $remove = auto_listings_option( 'delete_data' );
+if ( 'yes' !== $remove ) {
+	return;
+}
 
-if( $remove == 'yes' ) {
+$auto_listings_post_types = [ 'auto-listing', 'listing-enquiry' ];
+foreach ( $auto_listings_post_types as $post_type ) {
+	$items = get_posts( [
+		'post_type'   => $post_type,
+		'numberposts' => - 1,
+		'post_status' => [
+			'publish',
+			'pending',
+			'draft',
+			'auto-draft',
+			'future',
+			'private',
+			'inherit',
+			'trash',
+		],
+		'fields'      => 'ids',
+	] );
 
-	/** Delete All the Custom Post Types */
-	$auto_listings_post_types = array( 'auto-listing', 'listing-enquiry' );
-	foreach ( $auto_listings_post_types as $post_type ) {
-
-		$items = get_posts( array( 'post_type' => $post_type, 'numberposts' => -1, 'post_status' => array('publish', 'pending', 'draft', 'auto-draft', 'future', 'private', 'inherit', 'trash'), 'fields' => 'ids' ) );
-
-		if ( $items ) {
-			foreach ( $items as $item ) {
-				wp_delete_post( $item, true);
-			}
+	if ( $items ) {
+		foreach ( $items as $item ) {
+			wp_delete_post( $item, true );
 		}
 	}
+}
 
+/** Delete all the Plugin Options */
+delete_option( 'auto_listings_options' );
+delete_option( 'auto_listings_version' );
+delete_option( 'auto_listings_version_upgraded_from' );
 
+/** Delete Capabilities */
+$roles = new AutoListings\Roles();
+$roles->remove_caps();
 
-	/** Delete all the Plugin Options */
-	delete_option( 'auto_listings_options' );
-	delete_option( 'auto_listings_version' );
-	delete_option( 'auto_listings_version_upgraded_from' );
-
-	/** Delete Capabilities */
-	$roles = new AutoListings\Roles();
-	$roles->remove_caps();
-
-	/** Delete the Roles */
-	$auto_listings_roles = array( 'auto_listings_seller' );
-	foreach ( $auto_listings_roles as $role ) {
-		remove_role( $role );
-	}
-
+/** Delete the Roles */
+$auto_listings_roles = [ 'auto_listings_seller' ];
+foreach ( $auto_listings_roles as $role ) {
+	remove_role( $role );
 }
