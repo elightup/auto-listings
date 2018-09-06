5d8fc2b5bf0ce7933257beb571508c2f3db6485f
viet nguyen <viet34tqc@gmail.com>
2018-09-06 15:54:21 +0700

    fix lỗi ở trang author khi không active extension multidealer

--- src/Frontend/TemplateLoader.php
+++ src/Frontend/TemplateLoader.php
@@ -9,18 +9,6 @@ class TemplateLoader {
 	public function template_include( $template ) {
 		$file = '';
 
-		// Only show on sellers page.
-		if ( is_author() ) {
-			$user       = new \WP_User( auto_listings_seller_ID() );
-			$user_roles = $user->roles;
-
-			$listings_count = auto_listings_seller_listings_count( auto_listings_seller_ID() );
-
-			if ( in_array( 'auto_listings_seller', $user_roles ) || $listings_count > 0 ) {
-				$file = 'seller.php';
-			}
-		}
-
 		if ( is_single() && get_post_type() == 'auto-listing' ) {
 			$file = 'single-listing.php';
 		}
