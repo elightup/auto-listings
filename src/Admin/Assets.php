d22cc9b8ce889a518f22f3139968a052e6ce890e
viet nguyen <viet34tqc@gmail.com>
2018-11-13 17:04:25 +0700

    Update trang extensions trong Settings của plugin

--- src/Admin/Assets.php
+++ src/Admin/Assets.php
@@ -9,10 +9,12 @@ class Assets {
 	public function enqueue( $hook ) {
 		$css_dir = AUTO_LISTINGS_URL . 'assets/admin/css/';
 
-		if ( $hook !== 'profile.php' && $hook !== 'user-edit.php' && ! is_auto_listings_admin() ) {
+		if ( $hook !== 'profile.php' && $hook !== 'user-edit.php' && $hook !== 'auto-listing_page_auto-listings' && ! is_auto_listings_admin() ) {
 			return;
 		}
-
+		if ( $hook === 'auto-listing_page_auto-listings' ) {
+			wp_enqueue_style ( 'al-admin-extensions', $css_dir . 'extensions.css', AUTO_LISTINGS_VERSION );
+		}
 		wp_enqueue_style( 'al-admin', $css_dir . 'auto-listings.css', AUTO_LISTINGS_VERSION );
 	}
 }
