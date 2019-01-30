9801b4affe19bf505b483521111881488fb204d2
viet nguyen <viet34tqc@gmail.com>
2019-01-30 10:56:00 +0700

    Nếu là trường year thì sắp xếp lại từ cao xuống thấp

--- src/functions-general.php
+++ src/functions-general.php
@@ -330,7 +330,11 @@ function auto_listings_search_get_vehicle_data() {
 	foreach ( $data as $key => $value ) {
 		$data[ $key ] = array_filter( $data[ $key ] );
 		$data[ $key ] = array_unique( $data[ $key ] );
-		sort( $data[ $key] );
+		if ( 'year' === $key ) {
+			rsort( $data[ $key ] );
+		} else {
+			sort( $data[ $key ] );
+		}
 	}
 
 	return apply_filters( 'auto_listings_search_get_vehicle_data', $data );
