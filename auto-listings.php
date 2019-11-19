57d9f406f548e2fd35b27aab2258b1c858bf295f
viet nguyen <viet34tqc@gmail.com>
2019-11-19 09:42:27 +0700

    version 2.1.5

--- auto-listings.php
+++ auto-listings.php
@@ -5,7 +5,7 @@
  * Author: WP Auto Listings
  * Author URI: https://wpautolistings.com
  * Plugin URI: https://wpautolistings.com
- * Version: 2.1.4
+ * Version: 2.1.5
  * Text Domain: auto-listings
  * Domain Path: languages
  *
