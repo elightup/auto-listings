3bc29e50ee068c33ee90910e4227ba4b71695326
Latios Thinh <latiosthinh@live.com>
2020-07-15 12:47:03 +0700

    modify single-listing gallery
    making gallery works with optimize js

--- templates/single-listing/gallery.php
+++ templates/single-listing/gallery.php
@@ -15,10 +15,26 @@ $gallery = rwmb_meta( '_al_listing_image_gallery', [ 'size' => 'al-lge' ] );
 if ( empty( $gallery ) ) {
 	return;
 }
+
+$gallery_options = [
+	'gallery_mode'   => auto_listings_option( 'gallery_mode' ) ? auto_listings_option( 'gallery_mode' ) : 'fade',
+	'auto_slide'     => auto_listings_option( 'auto_slide' ) === 'no' ? false : true,
+	'slide_delay'    => auto_listings_option( 'slide_delay' ) ? auto_listings_option( 'slide_delay' ) : 3000,
+	'slide_duration' => auto_listings_option( 'slide_duration' ) ? auto_listings_option( 'slide_duration' ) : 3000,
+	'thumbs_shown'   => auto_listings_option( 'thumbs_shown' ) ? auto_listings_option( 'thumbs_shown' ) : 6,
+	'thumb_margin'   => auto_listings_option( 'thumb_margin' ) ? auto_listings_option( 'thumb_margin' ) : 10,
+];
+
 $new = auto_listings_highlight_new();
 ?>
 
-<div class="gallery-wrap">
+<div class="gallery-wrap"
+	<?php
+	foreach ( $gallery_options as $option_key => $option_value ) {
+		printf( ' data-%1s="%2s"', $option_key, $option_value, 'ecar' );
+	}
+	?>
+>
 
 	<?php if ( $new ) : ?>
 		<span style="background:<?php echo esc_attr( $new ); ?>;" class="highlight-new">
