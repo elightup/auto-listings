ff40d12c9dcc76bb6ca5121beabf172527cbcbaf
viet nguyen <viet34tqc@gmail.com>
2020-09-29 11:31:30 +0700

    Add Ajax submission for enquiry form

--- src/Enquiry/ContactForm.php
+++ src/Enquiry/ContactForm.php
@@ -30,7 +30,7 @@ class ContactForm {
 		$submit_button      = __( 'Send Enquiry', 'auto-listings' );
 		$recaptcha_site_key = auto_listings_option( 'captcha_site_key' );
 		$recaptcha_secret   = auto_listings_option( 'captcha_site_key' );
-		$shortcode          = sprintf( '[mb_frontend_form id="auto_listings_contact_form" submit_button="%s" recaptcha_key="%s" recaptcha_secret="%s"]', $submit_button, $recaptcha_site_key, $recaptcha_secret );
+		$shortcode          = sprintf( '[mb_frontend_form id="auto_listings_contact_form" ajax="true" submit_button="%s" recaptcha_key="%s" recaptcha_secret="%s"]', $submit_button, $recaptcha_site_key, $recaptcha_secret );
 		return do_shortcode( $shortcode );
 	}
 
