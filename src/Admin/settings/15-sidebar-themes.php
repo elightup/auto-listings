cb0e6303adb423b40498af987bc18dd1d751999c
viet nguyen <viet34tqc@gmail.com>
2020-10-20 14:27:46 +0700

    Update widget description and theme recommendation

--- src/Admin/settings/15-sidebar-themes.php
+++ src/Admin/settings/15-sidebar-themes.php
@@ -1,16 +1,21 @@
 <?php ob_start(); ?>
-<p><?php esc_html_e( 'Want a full-featured theme that is compatible with the plugin?', 'auto-listings' ); ?></p>
 <p>
 	<?php
 	/* translators: theme url */
-	echo wp_kses_post( sprintf( __( 'Check out %s - our <strong>free</strong> theme for car listings website.', 'auto-listings' ), '<a href="https://wpautolistings.com/themes/?utm_source=WordPress&utm_medium=link&utm_campaign=plugin" target="_blank">CarListings</a>' ) );
+	echo wp_kses_post( sprintf( __( '%s - our premium theme for car dealership and car listings websites.', 'auto-listings' ), '<a href="https://themeforest.net/item/corify-wordpress-car-listings-dealership-theme/25740868?ref=fitwp" target="_blank">Corify</a>' ) );
 	?>
-	</p>
+</p>
+<p>
+	<?php
+	/* translators: theme url */
+	echo wp_kses_post( sprintf( __( '%s - free theme that helps you build a listings website with ease.', 'auto-listings' ), '<a href="https://wpautolistings.com/themes/?utm_source=WordPress&utm_medium=link&utm_campaign=plugin" target="_blank">CarListings</a>' ) );
+	?>
+</p>
 <?php
 $message = ob_get_clean();
 return [
 	'id'             => 'sidebar-themes',
-	'title'          => __( 'Get A Theme', 'auto-listings' ),
+	'title'          => __( 'Recommended Themes', 'auto-listings' ),
 	'settings_pages' => 'auto-listings',
 	'context'        => 'side',
 	'fields'         => [
--- src/functions-sidebars.php
+++ src/functions-sidebars.php
@@ -33,7 +33,7 @@ function auto_listings_register_sidebars() {
 		array(
 			'name'          => __( 'Auto Listings Single', 'auto-listings' ),
 			'id'            => 'auto-listings-single',
-			'description'   => __( 'Widgets in this area will be shown on all single listing pages.', 'auto-listings' ),
+			'description'   => __( 'Use this widget to customize the sidebar on the single listing page.', 'auto-listings' ),
 			'before_widget' => '<div id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</div>',
 			'before_title'  => '<h3 class="single-listing__sidebar-heading">',
