c0e0c983f26d41fb4e67a567ee3d2db6834cb43c
Anh Tran <anhtnt@elightup.com>
2024-03-22 17:40:44 +0700

    Version 2.6.8

--- auto-listings.php
+++ auto-listings.php
@@ -5,7 +5,7 @@
  * Author:           WP Auto Listings
  * Author URI:       https://wpautolistings.com
  * Plugin URI:       https://wpautolistings.com
- * Version:          2.6.7
+ * Version:          2.6.8
  * Text Domain:      auto-listings
  * Domain Path:      languages
  * Requires Plugins: meta-box
