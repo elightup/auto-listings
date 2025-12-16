681f55fa93904911c9abd5a4e959954fe7501687
Bao <lexuanbao161096@gmail.com>
2025-12-16 11:15:53 +0700

    Version 2.7.2

--- auto-listings.php
+++ auto-listings.php
@@ -5,7 +5,7 @@
  * Author:           WP Auto Listings
  * Author URI:       https://wpautolistings.com
  * Plugin URI:       https://wpautolistings.com
- * Version:          2.7.1
+ * Version:          2.7.2
  * Text Domain:      auto-listings
  * License:          GPLv3
  *
